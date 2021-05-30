469032ecdcdb0fc99b6ae73b796c0349e240e8ce
Diyar Tesfaye <mr.vasya10.10@gmail.com>
2021-05-30 22:32:47 +0300

    added combobox

--- ChessWPF/MainWindow.xaml.cs
+++ ChessWPF/MainWindow.xaml.cs
@@ -12,17 +12,24 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using ChessLibrary;
 
 namespace ChessWPF
 {
-    /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             InitializeComponent();
+
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+        }
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
         }
     }
 }
